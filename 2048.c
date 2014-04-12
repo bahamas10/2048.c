d77ea3b1fe470017608f21d17b8039ab0fa531f0
Dave Eddy <dave@daveeddy.com>
2014-04-11 20:58:59 -0700

    updated comment

--- 2048.c
+++ 2048.c
@@ -75,7 +75,7 @@ int out_of_bounds(int x, int y);
 int has_moves_left();
 
 // fix the tty on exit
-void on_exit2() { /*Fixed to on_exit2 (on_exit is in stdlib.h*/
+void on_exit2() { /* Fixed to on_exit2 (on_exit is in non-posix stdlib.h) */
 	system("stty cooked");
 }
 
